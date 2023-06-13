1864e5d482f602b61a8204d982aeabffcc536996
Adam <adam.mcardlesep13@gmail.com>
2023-06-14 04:46:51 +1200

    Added a new option to make the enemy a stationary type
    Checking the box means the enemy cannot move, and will shoot the player if it shares an x or y with the enemy rather than if it is at its preffered distance

--- Unity Project Files/Assets/Scripts/EnemyController.cs
+++ Unity Project Files/Assets/Scripts/EnemyController.cs
@@ -9,13 +9,14 @@ public class EnemyController : MonoBehaviour
     public GameObject projectilePrefab; // Reference to the projectile prefab
     public int preferredDistance = 2;
     public LayerMask obstacleMask;
+    public bool isStationary; // Added boolean to control if the enemy is stationary
 
     public BeatController beatController;
 
     private GameObject player;
     private bool hasMovedOnThisBeat;
     private bool wasBeatOn;
-    
+
     private Vector3 previousMove;
 
     private EnemySpriteController enemySpriteController; // Reference to the EnemySpriteController
@@ -46,79 +47,26 @@ public class EnemyController : MonoBehaviour
 
     void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, movePoint, moveSpeed * Time.fixedDeltaTime);
-
-        if (Vector3.Distance(transform.position, movePoint) <= .05f)
+        if (!isStationary) // Enemy will only move if isStationary is false
         {
-            if (beatController.isBeatOn && !hasMovedOnThisBeat)
+            transform.position = Vector3.MoveTowards(transform.position, movePoint, moveSpeed * Time.fixedDeltaTime);
+
+            if (Vector3.Distance(transform.position, movePoint) <= .05f)
             {
-                Vector3 directionToPlayer = player.transform.position - transform.position;
-                Vector3 moveDirection = Vector3.zero;
-
-                float distanceX = Mathf.Abs(player.transform.position.x - transform.position.x);
-                float distanceY = Mathf.Abs(player.transform.position.y - transform.position.y);
-                bool sameX = Mathf.Approximately(transform.position.x, player.transform.position.x);
-                bool sameY = Mathf.Approximately(transform.position.y, player.transform.position.y);
-
-                // if on same X, maintain preferred distance on Y and vice versa
-                if (sameX)
+                if (beatController.isBeatOn && !hasMovedOnThisBeat)
                 {
-                    if (distanceY < preferredDistance)
-                        moveDirection = new Vector3(0, -Mathf.Sign(directionToPlayer.y), 0);
-                    else if (distanceY > preferredDistance)
-                        moveDirection = new Vector3(0, Mathf.Sign(directionToPlayer.y), 0);
+                    CalculateMoveDirection();
                 }
-                else if (sameY)
-                {
-                    if (distanceX < preferredDistance)
-                        moveDirection = new Vector3(-Mathf.Sign(directionToPlayer.x), 0, 0);
-                    else if (distanceX > preferredDistance)
-                        moveDirection = new Vector3(Mathf.Sign(directionToPlayer.x), 0, 0);
-                }
-                else
-                {
-                    if (distanceY < distanceX)
-                        moveDirection = new Vector3(0, Mathf.Sign(directionToPlayer.y), 0);
-                    else if (distanceX <= distanceY)
-                        moveDirection = new Vector3(Mathf.Sign(directionToPlayer.x), 0, 0);
-                }
-
-                if (!Physics2D.OverlapCircle(transform.position + moveDirection, .2f, whatStopsMovement))
-                {
-                    movePoint = transform.position + moveDirection; // Update the movePoint
-                    hasMovedOnThisBeat = true;
-                    
-                }
-
-                if ((distanceX <= preferredDistance && sameY) || (distanceY <= preferredDistance && sameX))
-                {
-                    if (canFireProjectile && previousMove == movePoint)
-                    {
-                        Vector2 directionToPlayerNormalized = (player.transform.position - transform.position).normalized;
-                        RaycastHit2D hit = Physics2D.Raycast(transform.position, directionToPlayerNormalized, preferredDistance, obstacleMask);
-
-                        if (hit.collider == null || hit.collider.gameObject == player)
-                        {
-                            // If the raycast doesn't hit any obstacle or it hits the player, then shoot
-                            enemySpriteController.SetFacingDirectionToPlayer(player); // change the sprite direction before shooting
-                            GameObject newProjectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
-                            newProjectile.GetComponent<EnemyProjectile>().direction = enemySpriteController.GetFacingDirection();
-                            activeProjectile = newProjectile;
-                            canFireProjectile = false;
-                        }
-                    }
-                }
-
             }
+        }
+        else // Even if the enemy is stationary, we still want it to fire projectiles
+        {
+            CheckAndFireProjectile();
         }
     }
 
     void Update()
     {
-
-        
-
-
         // Check if the beat just started
         if (beatController.isBeatOn && !wasBeatOn)
         {
@@ -129,23 +77,91 @@ public class EnemyController : MonoBehaviour
         // Update wasBeatOn to reflect the current state
         wasBeatOn = beatController.isBeatOn;
 
-    
         // Check if the active projectile is destroyed
         if (activeProjectile == null)
         {
             canFireProjectile = true; // Enable firing if there's no active projectile
-        } 
-
-        if(previousMove != movePoint){
-            
-            hasMovedOnThisBeat = true;
-
         }
 
+        if(previousMove != movePoint){
+            hasMovedOnThisBeat = true;
+        }
     }
 
     public bool HasMovedOnThisBeat()
     {
         return hasMovedOnThisBeat;
+    }
+
+    // Move the calculation of move direction to its own function
+    private void CalculateMoveDirection()
+    {
+        Vector3 directionToPlayer = player.transform.position - transform.position;
+        Vector3 moveDirection = Vector3.zero;
+
+        float distanceX = Mathf.Abs(player.transform.position.x - transform.position.x);
+        float distanceY = Mathf.Abs(player.transform.position.y - transform.position.y);
+        bool sameX = Mathf.Approximately(transform.position.x, player.transform.position.x);
+        bool sameY = Mathf.Approximately(transform.position.y, player.transform.position.y);
+
+        // if on same X, maintain preferred distance on Y and vice versa
+        if (sameX)
+        {
+            if (distanceY < preferredDistance)
+                moveDirection = new Vector3(0, -Mathf.Sign(directionToPlayer.y), 0);
+            else if (distanceY > preferredDistance)
+                moveDirection = new Vector3(0, Mathf.Sign(directionToPlayer.y), 0);
+        }
+        else if (sameY)
+        {
+            if (distanceX < preferredDistance)
+                moveDirection = new Vector3(-Mathf.Sign(directionToPlayer.x), 0, 0);
+            else if (distanceX > preferredDistance)
+                moveDirection = new Vector3(Mathf.Sign(directionToPlayer.x), 0, 0);
+        }
+        else
+        {
+            if (distanceY < distanceX)
+                moveDirection = new Vector3(0, Mathf.Sign(directionToPlayer.y), 0);
+            else if (distanceX <= distanceY)
+                moveDirection = new Vector3(Mathf.Sign(directionToPlayer.x), 0, 0);
+        }
+
+        if (!Physics2D.OverlapCircle(transform.position + moveDirection, .2f, whatStopsMovement))
+        {
+            movePoint = transform.position + moveDirection; // Update the movePoint
+            hasMovedOnThisBeat = true;
+        }
+
+        CheckAndFireProjectile();
+    }
+
+    // Move the check and fire projectile logic to its own function
+    private void CheckAndFireProjectile()
+    {
+        Vector3 directionToPlayer = player.transform.position - transform.position;
+        float distanceX = Mathf.Abs(player.transform.position.x - transform.position.x);
+        float distanceY = Mathf.Abs(player.transform.position.y - transform.position.y);
+        bool sameX = Mathf.Approximately(transform.position.x, player.transform.position.x);
+        bool sameY = Mathf.Approximately(transform.position.y, player.transform.position.y);
+
+        if (((distanceX <= preferredDistance && sameY) || (distanceY <= preferredDistance && sameX)) || ((sameX && isStationary) || (sameY && isStationary)))
+        {
+            if (canFireProjectile && previousMove == movePoint)
+            {
+                Vector2 directionToPlayerNormalized = (player.transform.position - transform.position).normalized;
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, directionToPlayerNormalized, preferredDistance, obstacleMask);
+
+                if (hit.collider == null || hit.collider.gameObject == player)
+                {
+                    // If the raycast doesn't hit any obstacle or it hits the player, then shoot
+                    enemySpriteController.SetFacingDirectionToPlayer(player); // change the sprite direction before shooting
+                    GameObject newProjectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+                    newProjectile.GetComponent<EnemyProjectile>().direction = enemySpriteController.GetFacingDirection();
+                    activeProjectile = newProjectile;
+                    canFireProjectile = false;
+                }
+            }
+        }
     }
 }

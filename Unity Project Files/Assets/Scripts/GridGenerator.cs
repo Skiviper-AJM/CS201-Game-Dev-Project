83f03085d1d8004b3fd8ac73aad5d067013599ab
Adam <adam.mcardlesep13@gmail.com>
2023-06-14 10:26:16 +1200

    Touched the holy grid generator
    added an option to generate a perimiter around the outside of it and adjust thiccness for aesthetic purpose

--- Unity Project Files/Assets/Scripts/GridGenerator.cs
+++ Unity Project Files/Assets/Scripts/GridGenerator.cs
@@ -1,10 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GridGenerator : MonoBehaviour
 {
     public GameObject gridTilePrefabA;
     public GameObject gridTilePrefabB;
+    public GameObject gridTilePrefabC; // The new prefab for the perimeter tiles
+    public bool generatePerimeter; // The boolean to control if the perimeter will be generated or not
     public int radius = 5;
+    public int perimeterThickness = 1; // The thickness of the perimeter
     public Transform centerObject;
 
     private void Start()
@@ -20,13 +25,22 @@ public class GridGenerator : MonoBehaviour
             return;
         }
 
-        for (int x = -radius; x <= radius; x++)
+        for (int x = -radius - perimeterThickness; x <= radius + perimeterThickness; x++)
         {
-            for (int y = -radius; y <= radius; y++)
+            for (int y = -radius - perimeterThickness; y <= radius + perimeterThickness; y++)
             {
                 Vector3 spawnPosition = centerObject.position + new Vector3(x, y, 0f);
+                GameObject gridTilePrefab;
 
-                GameObject gridTilePrefab = (x + y) % 2 == 0 ? gridTilePrefabA : gridTilePrefabB;
+                if (generatePerimeter && (x < -radius || x > radius || y < -radius || y > radius))
+                {
+                    gridTilePrefab = gridTilePrefabC;
+                }
+                else
+                {
+                    gridTilePrefab = (x + y) % 2 == 0 ? gridTilePrefabA : gridTilePrefabB;
+                }
+
                 Instantiate(gridTilePrefab, spawnPosition, Quaternion.identity, transform);
             }
         }
